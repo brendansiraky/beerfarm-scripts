9e8008766d46e13aaff389347120ca0b61ea948c
Brendan Hemat-Siraky <brendansirakydeveloper@gmail.com>
2025-04-09 15:05:12 +1000

    Removed some validation fields from schemas

--- src/validation.ts
+++ src/validation.ts
@@ -1,34 +1,6 @@
 import { z } from 'zod'
 
-const addressSchema = z.object({
-    companyName: z.string(),
-    contactName: z.string().optional(),
-    address1: z.string(),
-    suburb: z.string(),
-    state: z.object({
-        name: z.string(),
-        code: z.string(),
-    }),
-    postcode: z.string(),
-    country: z.object({
-        name: z.string(),
-        iso2Code: z.string(),
-        iso3Code: z.string(),
-    }),
-    location: z.object({
-        lat: z.string(),
-        lon: z.string(),
-    }),
-    enabled: z.boolean(),
-    approved: z.boolean(),
-    draft: z.boolean(),
-    starred: z.boolean(),
-    references: z.object({
-        numericId: z.string(),
-    }),
-    id: z.string(),
-})
-
+// Commented out fields for now as we don't actually care about them and this was causing issues in payload incoming.
 // Inbound Orders
 export const purchaseOrderSchema = z.object({
     details: z.object({
@@ -46,210 +18,30 @@ export const purchaseOrderSchema = z.object({
         'ALLOCATED',
         'REJECTED',
     ]),
-    customer: z.object({
-        enabled: z.boolean(),
-        name: z.string(),
-        id: z.string(),
-    }),
-    warehouse: z.object({
-        enabled: z.boolean(),
-        name: z.string(),
-        id: z.string(),
-    }),
-    version: z.number(),
     references: z.object({
         customer: z.string(),
-        numericId: z.string(),
+        // numericId: z.string(),
     }),
-    id: z.string(),
 })
 
+// Commented out fields for now as we don't actually care about them and this was causing issues in payload incoming.
 export const consignmentSchema = z.object({
     details: z.object({
-        type: z.string(),
-        authorityToLeave: z.boolean(),
         runsheet: z.object({
             date: z.string(),
             name: z.string(),
             id: z.string().uuid(),
         }),
-        deliveryRun: z.object({
-            warehouse: z.object({
-                enabled: z.boolean(),
-                name: z.string(),
-                id: z.string().uuid(),
-            }),
-            name: z.string(),
-            id: z.string().uuid(),
-        }),
-        collect: z.object({
-            address: addressSchema.extend({
-                phone: z.string().optional(),
-                properties: z
-                    .object({
-                        ins: z.string(),
-                        ot: z.string(),
-                        ct: z.string(),
-                    })
-                    .optional(),
-            }),
-        }),
-        deliver: z.object({
-            address: addressSchema.extend({
-                contactName: z.string().optional(),
-                properties: z
-                    .object({
-                        ins: z.string(),
-                        ot: z.string(),
-                        ct: z.string(),
-                    })
-                    .optional(),
-            }),
-            instructions: z.string(),
-        }),
-    }),
-    items: z.array(
-        z.object({
-            details: z.object({
-                product: z.object({
-                    name: z.string(),
-                    references: z.object({
-                        code: z.string(),
-                    }),
-                    id: z.string().uuid(),
-                }),
-                job: z
-                    .object({
-                        id: z.string().uuid(),
-                    })
-                    .optional(),
-            }),
-            type: z.string(),
-            measures: z.object({
-                weight: z.number().optional(),
-                quantity: z.number(),
-            }),
-            properties: z.object({
-                description: z.string(),
-            }),
-            id: z.string().uuid(),
-        })
-    ),
-    areChargesEditable: z.boolean(),
-    source: z.string(),
-    type: z.string(),
-    status: z.string(),
-    customer: z.object({
-        enabled: z.boolean(),
-        name: z.string(),
-        id: z.string().uuid(),
-    }),
-    warehouse: z.object({
-        enabled: z.boolean(),
-        name: z.string(),
-        id: z.string().uuid(),
-    }),
-    user: z.object({
-        name: z.string(),
-        id: z.string().uuid(),
-    }),
-    createDate: z.string(),
-    lastModified: z.string(),
-    measures: z.object({
-        distance: z.object({
-            value: z.number(),
-            unit: z.string(),
-        }),
-        hours: z.number(),
     }),
     references: z.object({
         customer: z.string(),
         numericId: z.string(),
     }),
-    properties: z.object({
-        'collect.cins': z.string(),
-        serviceType: z.string(),
-        'collect.cot': z.string(),
-        'collect.cct': z.string(),
-    }),
-    id: z.string().uuid(),
 })
 
+// Commented out fields for now as we don't actually care about them and this was causing issues in payload incoming.
 // Outbound Orders
 export const salesOrderSchema = z.object({
-    details: z.object({
-        urgent: z.boolean(),
-        invoiceValue: z.object({
-            amount: z.number(),
-            currency: z.string(),
-        }),
-        collect: z.object({
-            address: addressSchema.extend({
-                phone: z.string().optional(),
-                email: z.string().optional(),
-                properties: z
-                    .object({
-                        ins: z.string(),
-                        ot: z.string(),
-                        ct: z.string(),
-                    })
-                    .optional(),
-            }),
-        }),
-        deliver: z.object({
-            method: z.object({
-                type: z.string(),
-            }),
-            address: addressSchema.extend({
-                phone: z.string().optional(),
-                email: z.string().optional(),
-                properties: z
-                    .object({
-                        ins: z.string(),
-                        ot: z.string(),
-                        ct: z.string(),
-                    })
-                    .optional(),
-            }),
-            instructions: z.string(),
-        }),
-    }),
-    items: z.array(
-        z.object({
-            details: z.object({
-                product: z.object({
-                    customer: z.object({
-                        enabled: z.boolean(),
-                        name: z.string(),
-                        id: z.string().uuid(),
-                    }),
-                    name: z.string(),
-                    references: z.object({
-                        code: z.string(),
-                    }),
-                    id: z.string().uuid(),
-                }),
-                unitOfMeasure: z.object({
-                    type: z.string(),
-                    name: z.string(),
-                }),
-            }),
-            type: z.string(),
-            measures: z.object({
-                quantity: z.number(),
-            }),
-            references: z.object({
-                numericId: z.string(),
-            }),
-            properties: z.object({
-                sop_custom_field_1: z.string(),
-                sop_custom_field_2: z.string(),
-                sop_custom_field_3: z.string(),
-            }),
-            id: z.string().uuid(),
-        })
-    ),
-    type: z.string(),
     status: z.enum([
         'AWAITING_PICK_AND_PACK',
         'DISPATCHED',
@@ -258,22 +50,10 @@ export const salesOrderSchema = z.object({
         'PACKING_IN_PROGRESS',
         'REJECTED',
     ]),
-    customer: z.object({
-        enabled: z.boolean(),
-        name: z.string(),
-        id: z.string().uuid(),
-    }),
-    warehouse: z.object({
-        enabled: z.boolean(),
-        name: z.string(),
-        id: z.string().uuid(),
-    }),
-    version: z.number(),
     references: z.object({
         customer: z.string(),
-        numericId: z.string(),
+        // numericId: z.string(),
     }),
-    id: z.string().uuid(),
 })
 
 export type SalesOrder = z.infer<typeof salesOrderSchema>
